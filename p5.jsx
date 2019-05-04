0db11c09038665967dacb8aa838f9fde31d57cc6
Stephanie Zhang <zhang.steph@gmail.com>
2019-05-03 23:02:12 -0700

    finished part 5

--- p5.jsx
+++ p5.jsx
@@ -3,14 +3,22 @@ import ReactDOM from "react-dom";
 import { HashRouter, Route, Link } from "react-router-dom";
 
 import Header from "./components/header/Header";
-import ViewSwitcher from "./components/ViewSwitcher";
+import Example from "./components/example/Example";
+import States from "./components/states/States";
 
 ReactDOM.render(
   <HashRouter>
-    <Header />
-    <Route path="/states" component={States} />
-    <Route path="/example" component={Example} />
-    ...
+    <div>
+      <Header />
+      <div>
+        <Link to="/states">States</Link>{" "}
+      </div>
+      <div>
+        <Link to="/example">Example</Link>
+      </div>
+      <Route path="/states" component={States} />
+      <Route path="/example" component={Example} />
+    </div>
   </HashRouter>,
   document.getElementById("reactapp")
 );
